4d9ab1e98321b8e05d62c8302429c5160eb44216
minhchase1 <minhnqse181684@fpt.edu.vn>
2025-06-28 21:47:56 +0700

    CSS trang hướng dẫn cai thuốc, trông chuyên nghiệp hơn

--- BreathingFree/backend/obj/Debug/net9.0/BreathingFree.AssemblyInfo.cs
+++ BreathingFree/backend/obj/Debug/net9.0/BreathingFree.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BreathingFree")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85365835b852f595a6aeb124dba3843cb234a72d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BreathingFree")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BreathingFree")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
